dd55f64790a5e7bd467d8e2d75bb588c0f456c14
micky <m-pathania@github.com>
2021-05-14 22:05:20 +0530

    returns external load due to distributed load

---
+++ ext_load.m
@@ -0,0 +1,17 @@
+% Applies the external loads and moments at specified GDOF
+% ext_load(Load_Vector, External_Point_Load, External_Moment)
+function F = ext_load(F,P,M) 
+for ii = 1:size(P,1)
+    if(P(ii,2)~=0)
+    n1 = P(ii,1);
+    F(2*n1-1) = F(2*n1-1) + P(ii,2);
+    end
+end
+for ii = 1:size(M,1)
+    if(M(ii,2)~=0)
+    n1 = M(ii,1);
+    F(2*n1) = F(2*n1) + M(ii,2);
+    end
+end
+end
+
